52f91452fe6a2cf7203b92d0d7a56901ed89067e
Muaaz Saleem <muaazsaleem@outlook.com>
2014-08-29 13:16:54 +0500

    added my_map my_inject

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -1,4 +1,4 @@
-module Enumerable
+module MyEnumerable
 	def my_each collection
 		
 		if collection.class == Array
@@ -66,13 +66,46 @@ module Enumerable
 		end
 		bool
 	end
+
+	def my_count (collection)
+		count = 0
+		
+		my_each(collection) do |key, value|
+			if  yield(key,value)
+				count+=1
+			end
+		end
+		count
+	end
+
+	def my_map collection
+		container = collection.class.new
+		my_each(collection) do |key, value|
+			if yield(key,value)
+				container.push yield(key,value) if container.class == Array
+				container[key] = yield(key,value) if container.class == Hash
+			end
+		end
+		container
+	end
+
+	def my_inject collection, initial_value
+		temp = initial_value
+		my_each(collection) do |key, value|
+			yield_result = yield(temp, key, value)
+			if yield_result
+				temp =  yield_result 
+			end
+		end
+		temp
+	end
 end
 
 
 
 
 
-include Enumerable
+include MyEnumerable
 array = [1,2,3]
 hash = {"one" => "uno", "two" => "dos", "three" => "tres"}
 my_each(array) {|c| puts c}
@@ -91,3 +124,13 @@ p my_all?(hash) {|k,v| k== "two" }
 
 p my_none?(array) { |i| i%2 == 0 }
 p my_none?(hash) {|k,v| k== "two" }
+
+p my_count(array) { |i| i%2 == 0 }
+p my_count(hash) {|k,v| k== "two" }
+
+p my_map(array){|num| num**2 }
+p my_map(hash){|k,v| v+"yolo" }
+
+p my_inject(array,1){|num,element| num*=element }
+
+p my_inject(hash,0){|k,v| v.length }
